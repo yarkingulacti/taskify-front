b991b5ca777b35de25fa71f211ea2b4ebf191aa7
Yarkın Gülaçtı <yarkingulacti@outlook.com>
2024-06-09 04:10:30 +0300

    tricl

--- src/components/layout/menu/sidebar/Sidebar.tsx
+++ src/components/layout/menu/sidebar/Sidebar.tsx
@@ -1,24 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import Logo from "../../../../assets/images/logos/taskify.svg";
+import Logo from "../../../../assets/images/logo/logo-no-background.png";
 import { DefaultListRenderer } from "../../DefaultListRenderer";
+import { useNavigate } from "react-router-dom";
 
 export const Sidebar: React.FC<{
   links: Array<React.ReactNode>;
 }> = ({ links }) => {
+  const navigate = useNavigate();
   return (
-    <div className="flex h-screen flex-col shadow-lg w-2/12 justify-start border-e border-blue-100 bg-white">
-      <Link
-        to="/"
-        className="cursor-pointer flex flex-col items-center justify-center mt-3 mb-6"
-      >
-        {/* <Link to="/"> */}
-        <img src={Logo} alt="Taskify App Logo" />
-        <h1 className="text-xl font-semibold -mt-6 m-2">Taskify</h1>
-        <p className="text-center italic font-medium text-sm">
-          An app that exists for Tasks
-        </p>
-      </Link>
+    <div className="flex h-full flex-col shadow-lg w-2/12 justify-start border-e border-blue-100 bg-white">
+      {/* <Link to="/"> */}
+      <img
+        src={Logo}
+        alt="Taskify App Logo"
+        className="cursor-pointer h-1/5 mx-auto my-4"
+        onClick={() => navigate("/")}
+      />
       <ul className="border-t border-gray-300 py-4 pl-4">
         <DefaultListRenderer
           list={links}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,17 +1,17 @@
-import Banner from "../assets/images/banner.svg";
-import { RiBrainLine } from "react-icons/ri";
+import Banner from "../assets/images/banner.jpg";
+import Logo from "../assets/images/logo/logo-black.png";
 
 export function Home() {
   return (
-    <div className="flex flex-col items-center justify-center">
-      <h1 className="text-4xl flex items-center mt-5 uppercase">
-        <RiBrainLine className="mr-1 text-yellow-300" />{" "}
-        <span className="underline underline-offset-4">Taskify</span>
-      </h1>
-      <p className="text-xl mt-5 italic font-medium">
-        "Task"ing at it's finest!
-      </p>
-      <img src={Banner} alt="Taskify App Banner" className="w-6/12" />
+    <div className="flex gap-6 items-center justify-center">
+      <img src={Banner} alt="Taskify App Banner" className="w-9/12" />
+      <div className="flex flex-col items-center justify-center">
+        <img src={Logo} alt="Taskfiy app logo" className="w-full" />
+        <p className="text-xl font-light mt-2 text-center">
+          Taskify is a simple task manager that helps you keep track of your
+          daily tasks.
+        </p>
+      </div>
     </div>
   );
 }
